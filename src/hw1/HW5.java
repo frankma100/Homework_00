430a5ba3b8c5ce0f266b9d8dafe0f53bd5cd71b8
Frank Ma <frankma10000@gmail.com>
2023-09-05 21:48:31 +0800

    push HW1&2 to GitHub

--- src/hw1/HW5.java
+++ src/hw1/HW5.java
@@ -3,13 +3,12 @@ package hw1;
 public class HW5 {
 
 	public static void main(String[] args) {
+		double pv = 1500000;
+		final double rate = 0.02;
+		int year = 10;
 		
-		double m = 1_500_000,p=1.02,y=10;
-		for(double f = 0;f <= 10; f++);
-			
-			f=m*p*=10;
-		
-						System.out.println(f);
+		double fv = pv * Math.pow((1 +rate), year);
+		System.out.printf("年金終值:%.4f%n", fv);
 
 	}
 
--- src/hw2/HW1.java
+++ src/hw2/HW1.java
@@ -3,8 +3,14 @@ package hw2;
 public class HW1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-123456
+		
+		int sum = 0;
+		
+		for(int i = 2; i <= 1000; i+=2){
+			sum += i;
+			
+		}
+		System.out.println(sum);
 	}
 
 }
